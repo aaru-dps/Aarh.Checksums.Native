1407e647a88827ba1ca0ae584c0593bf160f61bf
Natalia Portillo <claunia@claunia.com>
2021-09-22 00:46:07 +0100

    Add SpamSum.

---
+++ spamsum.h
@@ -0,0 +1,63 @@
+/*
+ * This file is part of the Aaru Data Preservation Suite.
+ * Copyright (c) 2019-2021 Natalia Portillo.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define SPAMSUM_LENGTH 64
+#define NUM_BLOCKHASHES  31
+#define ROLLING_WINDOW   7
+#define HASH_INIT         0x28021967
+#define HASH_PRIME        0x01000193
+#define MIN_BLOCKSIZE    3
+#define FUZZY_MAX_RESULT  ((2 * SPAMSUM_LENGTH) + 20)
+
+typedef        struct
+{
+    uint32_t   H;
+    uint32_t   Halfh;
+    uint8_t Digest[SPAMSUM_LENGTH];
+    uint8_t Halfdigest;
+    uint32_t Dlen;
+} BlockhashContext;
+
+typedef        struct
+{
+    uint8_t Window[ROLLING_WINDOW];
+    uint32_t H1;
+    uint32_t H2;
+    uint32_t H3;
+    uint32_t N;
+}RollState;
+
+typedef        struct
+{
+    uint32_t               Bhstart;
+    uint32_t               Bhend;
+    BlockhashContext Bh[NUM_BLOCKHASHES];
+    uint64_t     TotalSize;
+    RollState Roll;
+} spamsum_ctx;
+
+spamsum_ctx* spamsum_init(void);
+int spamsum_update(spamsum_ctx* ctx, const uint8_t* data, uint32_t len);
+uint8_t* spamsum_final(spamsum_ctx* ctx);
+void spamsum_free(spamsum_ctx* ctx);
+
+void fuzzy_engine_step(spamsum_ctx* ctx, uint8_t c);
+void roll_hash(spamsum_ctx* ctx, uint8_t c);
+void fuzzy_try_reduce_blockhash(spamsum_ctx* ctx);
+void fuzzy_try_fork_blockhash(spamsum_ctx* ctx);
+
