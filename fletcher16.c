4e7fbac91bed687fbe191ec00a6c12722797401d
Natalia Portillo <claunia@claunia.com>
2021-09-21 23:59:19 +0100

    Add Fletcher-16.

---
+++ fletcher16.c
@@ -0,0 +1,66 @@
+/*
+ * This file is part of the Aaru Data Preservation Suite.
+ * Copyright (c) 2019-2021 Natalia Portillo.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "library.h"
+
+#include "fletcher16.h"
+
+AARU_EXPORT fletcher16_ctx* AARU_CALL fletcher16_init()
+{
+    fletcher16_ctx* ctx;
+
+    ctx = (fletcher16_ctx*)malloc(sizeof(fletcher16_ctx));
+
+    if(!ctx) return NULL;
+
+    ctx->sum1 = 0xFF;
+    ctx->sum2 = 0xFF;
+
+    return ctx;
+}
+
+AARU_EXPORT int AARU_CALL fletcher16_update(fletcher16_ctx* ctx, const uint8_t* data, uint32_t len)
+{
+    if(!ctx || !data) return -1;
+
+    for(uint32_t i = 0; i < len; i++)
+    {
+        ctx->sum1 = (ctx->sum1 + data[i]) % FLETCHER16_MODULE;
+        ctx->sum2 = (ctx->sum2 + ctx->sum1) % FLETCHER16_MODULE;
+    }
+
+    return 0;
+}
+
+AARU_EXPORT int AARU_CALL fletcher16_final(fletcher16_ctx* ctx, uint16_t* checksum)
+{
+    if(!ctx) return -1;
+
+    *checksum = (ctx->sum2 << 8) | ctx->sum1;
+    return 0;
+}
+
+AARU_EXPORT void AARU_CALL fletcher16_free(fletcher16_ctx* ctx)
+{
+    if(!ctx) return;
+
+    free(ctx);
+}
---
+++ fletcher16.h
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the Aaru Data Preservation Suite.
+ * Copyright (c) 2019-2021 Natalia Portillo.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef AARU_CHECKSUMS_NATIVE_FLETCHER16_H
+#define AARU_CHECKSUMS_NATIVE_FLETCHER16_H
+
+#define FLETCHER16_MODULE 0xFF
+
+typedef struct {
+    uint8_t sum1;
+    uint8_t sum2;
+} fletcher16_ctx;
+
+AARU_EXPORT fletcher16_ctx* AARU_CALL fletcher16_init();
+AARU_EXPORT int AARU_CALL fletcher16_update(fletcher16_ctx* ctx, const uint8_t* data, uint32_t len);
+AARU_EXPORT int AARU_CALL fletcher16_final(fletcher16_ctx* ctx, uint16_t* checksum);
+AARU_EXPORT void AARU_CALL fletcher16_free(fletcher16_ctx* ctx);
+
+#endif//AARU_CHECKSUMS_NATIVE_FLETCHER16_H
