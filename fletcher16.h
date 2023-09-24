89382334ec52893d68ce4a84c629bb3cdb1f8d40
JosJuice <josjuice@gmail.com>
2023-09-24 09:36:14 +0200

    Implement Fletcher-16 using AVX2 instructions

--- fletcher16.h
+++ fletcher16.h
@@ -37,6 +37,7 @@ AARU_EXPORT void AARU_CALL fletcher16_free(fletcher16_ctx *ctx);
 #if defined(__x86_64__) || defined(__amd64) || defined(_M_AMD64) || defined(_M_X64) || defined(__I386__) ||            \
     defined(__i386__) || defined(__THW_INTEL) || defined(_M_IX86)
 
+AARU_EXPORT TARGET_WITH_AVX2 void AARU_CALL  fletcher16_avx2(uint8_t* sum1, uint8_t* sum2, const uint8_t* data, long len);
 AARU_EXPORT TARGET_WITH_SSSE3 void AARU_CALL fletcher16_ssse3(uint8_t* sum1, uint8_t* sum2, const uint8_t* data, long len);
 
 #endif
